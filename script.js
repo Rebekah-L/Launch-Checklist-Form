189145d36fbce9ee11351ad91f223c12f4026877
unknown <rebekah-l@outlook.com>
2019-11-19 10:19:31 -0600

    updated js to validate each field in the form and minor updates to index.html.

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 // Write your JavaScript code here!
 window.addEventListener("load", function(){
-   let form = document.querySelector("launchForm");
-   form.addEventListener("submit", function(event){
+   let button = document.getElementById("formSubmit")
+   button.addEventListener("click", function(event){
       let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
       if (pilotName.value==="" || copilotName.value==="" || fuelLevel.value==="" || cargoMass.value==="") {
          window.alert("All fields are required!");
+         event.preventDefault();
+      } else if (!isNaN(pilotName.value)){
+         window.alert("No numbers allowed in the Pilot Name field.")
+         event.preventDefault();
+      } else if (!isNaN(copilotName.value)){
+         window.alert("No numbers allowed in the Co-Pilot Name field.")
+         event.preventDefault();
+      } else if (isNaN(fuelLevel.value)){
+         window.alert("Expect a number in the Fuel Level field.")
+         event.preventDefault();
+      } else if (isNaN(cargoMass.value)){
+         window.alert("Expect a number in the Cargo Mass field.")
          event.preventDefault();
       };
       })
